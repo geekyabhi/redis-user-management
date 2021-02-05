4eca95df00df6f49ebccd558a6b74389749341e6
Abhinav Singh <thakurabhinav17122001@gmail.com>
2021-02-05 14:06:07 +0530

    Redis added

--- app.js
+++ app.js
@@ -25,48 +25,48 @@ app.get('/',(req,res,next)=>{
     res.render('searchusers')
 })
 
-// app.post('/user/search',(req,res,next)=>{
-//     let id=req.body.id
-//     client.hgetall(id,(err,obj)=>{
-//         if(!obj){
-//             res.render('searchusers',{
-//                 error:'Uset does not exist'
-//             })
-//         }else{
-//             obj.id=id
-//             res.render('details',{
-//                 user:obj
-//             })
-//         }
-//     })
-// })
+app.post('/user/search',(req,res,next)=>{
+    let id=req.body.id
+    client.hgetall(id,(err,obj)=>{
+        if(!obj){
+            res.render('searchusers',{
+                error:'Uset does not exist'
+            })
+        }else{
+            obj.id=id
+            res.render('details',{
+                user:obj
+            })
+        }
+    })
+})
 
 app.get('/user/add',(req,res)=>{
     res.render('adduser')
 })
 
-// app.post('/user/add',(req,res)=>{
-//     let id=req.body.id
-//     let first_name=req.body.first_name
-//     let last_name=req.body.last_name
-//     let email=req.body.email
-//     let phone=req.body.phone
+app.post('/user/add',(req,res)=>{
+    let id=req.body.id
+    let first_name=req.body.first_name
+    let last_name=req.body.last_name
+    let email=req.body.email
+    let phone=req.body.phone
 
-//     client.HMSET(id,[
-//         'first_name',first_name,
-//         'last_name',last_name,
-//         'phone',phone,
-//         'email',email
-//     ],(err,reply)=>{
-//         if(err){console.log(err)}
-//         else{console.log(reply)}
-//         res.redirect('/')
-//     })
-// })
+    client.HMSET(id,[
+        'first_name',first_name,
+        'last_name',last_name,
+        'phone',phone,
+        'email',email
+    ],(err,reply)=>{
+        if(err){console.log(err)}
+        else{console.log(reply)}
+        res.redirect('/')
+    })
+})
 
-// app.delete('/user/delete/:id',(req,res)=>{
-//     client.DEL(req.params.id)
-//     res.redirect('/')
-// })
+app.delete('/user/delete/:id',(req,res)=>{
+    client.DEL(req.params.id)
+    res.redirect('/')
+})
 
 app.listen(PORT,()=>{console.log('Server started on port',PORT)})
